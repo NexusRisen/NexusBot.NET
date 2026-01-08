0dab97223566c7b3e4ca33290af8c2cf4ed9eba2
PokeBot <noreply@local>
2026-01-08 17:19:04 -0500

    Refactor: Use InformationalVersion for version display

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 namespace SysBot.Pokemon.Helpers
 {
     public static class PokeBot
@@ -10,7 +12,18 @@ namespace SysBot.Pokemon.Helpers
         {
             get
             {
-                var v = System.Reflection.Assembly.GetEntryAssembly()?.GetName().Version;
+                var assembly = System.Reflection.Assembly.GetEntryAssembly();
+                if (assembly == null) return "vUnknown";
+
+                var attr = assembly.GetCustomAttribute<System.Reflection.AssemblyInformationalVersionAttribute>();
+                if (attr != null)
+                {
+                    var infoVer = attr.InformationalVersion;
+                    int plusIndex = infoVer.IndexOf('+');
+                    return "v" + (plusIndex > 0 ? infoVer.Substring(0, plusIndex) : infoVer);
+                }
+
+                var v = assembly.GetName().Version;
                 return v != null ? $"v{v.Major}.{v.Minor}.{v.Build}" : "vUnknown";
             }
         }
