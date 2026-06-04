7a9b5a7b833524195adb01e8f0758c80e7910c62
NexusRisen <bdkohn91@gmail.com>
2026-06-04 02:14:01 -0400

    test: add multi-Pokemon batch generation test

--- SysBot.Tests/BatchTradeTests.cs
+++ SysBot.Tests/BatchTradeTests.cs
@@ -1,6 +1,10 @@
 using FluentAssertions;
+using PKHeX.Core;
+using PKHeX.Core.AutoMod;
+using SysBot.Pokemon;
 using SysBot.Pokemon.Helpers;
 using Xunit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,6 +12,8 @@ namespace SysBot.Tests;
 
 public class BatchTradeTests
 {
+    static BatchTradeTests() => AutoLegalityWrapper.EnsureInitialized(new Pokemon.LegalitySettings());
+
     [Fact]
     public void ParseBatchTradeContent_SplitsOnDelimiters()
     {
@@ -41,5 +47,52 @@ public class BatchTradeTests
         
         result.Should().Contain("Alcremie-Ruby-Cream");
         result.Should().Contain(".FormArgument=0");
+    }
+
+    [Fact]
+    public void CanGenerateBatch()
+    {
+        string batchContent = @"
+Pikachu
+Level: 50
+Met Date: 20240101
+---
+Eevee
+Level: 10
+Scale: XXXS
+---
+Ditto
+Level: 100
+";
+        // Follow the bot's flow: Normalize -> Split -> Generate
+        var normalizedContent = BatchNormalizer.NormalizeBatchCommands(batchContent);
+        var sets = TradeModuleHelpers.ParseBatchTradeContent(normalizedContent);
+        
+        sets.Should().HaveCount(3);
+
+        var sav = AutoLegalityWrapper.GetTrainerInfo<PK8>();
+        foreach (var setStr in sets)
+        {
+            var s = new ShowdownSet(setStr);
+            var template = AutoLegalityWrapper.GetTemplate(s);
+            var pk = (PK8)sav.GetLegal(template, out _)!;
+            
+            pk.Should().NotBeNull();
+            new LegalityAnalysis(pk).Valid.Should().BeTrue();
+            
+            if (setStr.Contains("Pikachu"))
+            {
+                // Verify Met Date was applied correctly (normalized to .MetDate=20240101)
+                var date = (DateOnly)pk.MetDate;
+                int dateValue = date.Year * 10000 + date.Month * 100 + date.Day;
+                dateValue.Should().Be(20240101);
+            }
+            if (setStr.Contains("Eevee"))
+            {
+                // Scale: XXXS in SV is .Scale=0, in SWSH it maps to HeightScalar
+                // Just verify it's legal for now as property names differ between gen 8/9
+                new LegalityAnalysis(pk).Valid.Should().BeTrue();
+            }
+        }
     }
 }
