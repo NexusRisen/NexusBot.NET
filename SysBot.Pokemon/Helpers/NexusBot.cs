a04574551911c12b77e824a0821272cdaea3f0c2
NexusRisen <bdkohn91@gmail.com>
2026-07-20 17:13:52 -0400

    Bump version to 9.1.12

--- SysBot.Pokemon/Helpers/NexusBot.cs
+++ SysBot.Pokemon/Helpers/NexusBot.cs
@@ -3,7 +3,7 @@ namespace SysBot.Pokemon.Helpers
     public static class NexusBot
     {
         public static string Name { get; set; } = "NexusBot";
-        public static string Version => "9.1.11";
+        public static string Version => "9.1.12";
         public const string ConfigPath = "config.json";
     }
 }
